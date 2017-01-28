3ff137c326f1a470555768ca8936fb578f434ef2
Tony Brock <abrock17@gmail.com>
2017-01-28 11:57:30 -0500

    more erasing - only the last occurence now

--- src/main/groovy/com/mcsearchin/pencil/Eraser.groovy
+++ src/main/groovy/com/mcsearchin/pencil/Eraser.groovy
@@ -3,8 +3,12 @@ package com.mcsearchin.pencil
 class Eraser {
 
     def erase(String text, Paper paper) {
-        (0..<paper.text.length()).each {
-            paper.erase(it)
+        def start = paper.text.lastIndexOf(text)
+        if (start >= 0) {
+            def end = start + text.length()
+            (start..<end).each {
+                paper.erase(it)
+            }
         }
     }
 }
--- src/test/groovy/com/mcsearchin/pencil/EraserSpec.groovy
+++ src/test/groovy/com/mcsearchin/pencil/EraserSpec.groovy
@@ -11,10 +11,10 @@ class EraserSpec extends Specification {
         paper = new Paper()
     }
 
-    public "when it is instructed to erase text, the text is replaced with whitespace"() {
+    public "when it is instructed to erase all of text, the text is replaced with whitespace"() {
         given:
         subject = new Eraser()
-        def text = 'xy'
+        def text = 'yo'
         write(text, paper)
 
         when:
@@ -22,6 +22,43 @@ class EraserSpec extends Specification {
 
         then:
         paper.text == '  '
+    }
+
+    public "when it is instructed to erase only some of text, the specified text is replaced with whitespace"() {
+        given:
+        subject = new Eraser()
+        write('you', paper)
+
+        when:
+        subject.erase('yo', paper)
+
+        then:
+        paper.text == '  u'
+    }
+
+    public "when it is instructed to erase text not on the paper, the text on the paper is unaltered"() {
+        given:
+        subject = new Eraser()
+        def text = 'yo'
+        write(text, paper)
+
+        when:
+        subject.erase('ho', paper)
+
+        then:
+        paper.text == text
+    }
+
+    public "when the text to be erased occurs more than once, only the last occurrence is erased"() {
+        given:
+        subject = new Eraser()
+        write('yo yo yo', paper)
+
+        when:
+        subject.erase('yo', paper)
+
+        then:
+        paper.text == 'yo yo   '
     }
 
     private write(String text, Paper paper) {
